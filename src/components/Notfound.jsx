e52bd76d7bb4cb401d5ff84e63d959faa64112cd
zahramagdi15@gmail.com <zahramagdi15@gmail.com>
2025-06-19 03:35:38 +0300

    add not foud page

--- src/components/Notfound.jsx
+++ src/components/Notfound.jsx
@@ -1,10 +1,38 @@
-import React from 'react'
-
+import React from "react";
+import { Box, Typography, Button, Container } from "@mui/material";
+import svg404 from "../assets/images/404-error.png";
 export default function Notfound() {
   return (
-    <div>
-      <h1>404 - Not Found</h1>
-      <p>The page you are looking for does not exist.</p>
-    </div>
-  )
+    <Container
+      maxWidth="md"
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        height: "100vh",
+        textAlign: "center",
+      }}
+    >
+      <Box
+        component="img"
+        src={svg404}
+        alt="404 Not Found"
+        sx={{
+          maxWidth: "100%",
+          height: "auto",
+          mb: 4,
+        }}
+      />
+
+      <Button
+        variant="text"
+        color="black"
+        href="/home"
+        sx={{fontSize:"30px" ,textDecoration:"underline" }}
+      >
+        went to home page
+      </Button>
+    </Container>
+  );
 }
